08cf2e87c3422956e6c1dc3283b72cf4f1cd79ed
Ryan <rrpizani@gmail.com>
2021-08-19 10:58:55 -0300

    Aula do bloco 152

---
+++ Bloco152.js
@@ -0,0 +1,22 @@
+const nomes = [
+  "Ryan Pizani",
+  "Gabriel Bello",
+  "Leonardo Aguiar",
+  "Humberto Silva",
+];
+
+const apenasComE = nomes.filter((n) => n.startsWith("G"));
+console.log(apenasComE);
+
+const primeiras = nomes.map((n) => n.charAt(0));
+console.log(primeiras);
+
+const todosComO = nomes.every((n) => n.startsWith("i"));
+console.log(todosComO);
+
+const numeros = [1, 2, 3, 4, 5];
+const soma = numeros.reduce((ac, v) => ac + v);
+console.log(soma);
+
+const nomesComO = nomes.filter((n) => n.indexOf("o") > -1);
+console.log(nomesComO);
